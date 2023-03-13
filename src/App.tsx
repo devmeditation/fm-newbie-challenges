98e94cd7fccfb90f19f01483fc6f4d2b8dd8663b
Dev Meditation <devmeditation@bcl-st.com.br>
2023-03-13 11:20:14 -0300

    Project 006: Stats preview card

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import { ProductPreviewCard } from "./ProductPreviewCard";
 import { ProfileCard } from "./ProfileCard";
 import { ResultsSummary } from "./ResultsSummary";
 import { SkilledELearning } from "./SkilledELearning";
+import { StatsPreviewCard } from "./StatsPreviewCard";
 import { WorkitLandingPage } from "./WorkitLandingPage";
 
 function App() {
   return (
     <>
-      {/* <ProductPreviewCard />
-      <SkilledELearning />
-      <ResultsSummary />
-      <WorkitLandingPage /> */}
-      <ProfileCard />
-
+      {/* <ProductPreviewCard /> */}
+      {/* <SkilledELearning /> */}
+      {/* <ResultsSummary /> */}
+      {/* <WorkitLandingPage /> */}
+      {/* <ProfileCard /> */}
+      <StatsPreviewCard />
     </>
   );
 }
---
+++ src/StatsPreviewCard/index.tsx
@@ -0,0 +1,48 @@
+import styles from "./statsPreviewCard.module.css";
+
+export function StatsPreviewCard() {
+  return (
+    <section className={styles.statsPreviewCard}>
+      <div className={styles.container}>
+        <div className={styles.image}>
+          <span></span>
+          <img
+            src="/statsPreviewCard.jpg"
+            alt="Grupo de pessoas em uma mesa de reunião"
+          />
+        </div>
+
+        <div className={styles.description}>
+          <div className={styles.text}>
+            <h1>
+              Obtenha <span> insights </span> que ajudam sua empresa a crescer.
+            </h1>
+
+            <p>
+              Descubra os benefícios da análise de dados e tome melhores
+              decisões em relação à receita, experiência do cliente e eficiência
+              geral.
+            </p>
+          </div>
+
+          <div className={styles.footer}>
+            <div className={styles.numbers}>
+              <strong>10K+</strong>
+              <span>empresas</span>
+            </div>
+
+            <div className={styles.numbers}>
+              <strong>314</strong>
+              <span> modelos</span>
+            </div>
+
+            <div className={styles.numbers}>
+              <strong>12M+</strong>
+              <span>consultas</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
